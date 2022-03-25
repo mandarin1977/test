6c29fe37d3f25412a9024b7a41f62d40fa2c1dd2
mandarin1977 <57694287+mandarin1977@users.noreply.github.com>
2022-03-25 13:49:44 +0900

    Add files via upload

---
+++ js/ex2js.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+$('.mainSliderw').slick({
+	prevArrow :'<button type="button" class="slick-prev">&lt;</button>',
+	nextArrow : '<button type="button" class="slick-next">&gt;</button>',
+	autoplay : true,
+	autoplaySpeed : 3000,
+});
