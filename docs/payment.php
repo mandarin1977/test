293067c0852f4497e2530ef3b8e0b8d3e5e766ad
mandarin1977 <57694287+mandarin1977@users.noreply.github.com>
2025-05-17 20:02:50 +0900

    Add files via upload

---
+++ docs/payment.php
@@ -0,0 +1,17 @@
+<div id="payment" class="payment paymentstyle001">
+    <header>
+        <nav>
+            <p></p>
+            <button></button>
+        </nav>
+    </header>
+    <main>
+        <article>
+
+        </article>
+        <div>
+            <button>취소</button>
+            <button class="button_style_red">결제하기</button>
+        </div>
+    </main>
+</div>
